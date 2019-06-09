0f78a2b0c841c361f4d1d3ec3765225f9f1c54c2
Tim Ng <ncy.timothy@gmail.com>
2019-06-09 11:25:45 +0800

    feat: First Part done

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        Text("Hello Tim!")
+            .font(.title) // these are called modifiers
+            .color(.green) // each modifier creates a new view.
+                // There is an inherited option in the inspector of the canvas that will
+                // default the color to black. As if removing the .color(.green) modifier
     }
 }
 
---
+++ Notes.md
@@ -0,0 +1,12 @@
+#  Notes
+```
+
+var body: some View {
+    Text("Hello Tim!")
+        .font(.title) // these are called modifiers
+            .color(.green) // each modifier creates a new view.
+                    // There is an inherited option in the inspector of the canvas that will
+                    // default the color to black. As if removing the .color(.green) modifier
+}
+```
+
